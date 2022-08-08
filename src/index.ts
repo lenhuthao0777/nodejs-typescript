013a123a2cfbb532e14100d5998446d0c1c8fa2c
haolegumi <hao.le@gumiviet.com>
2022-08-08 16:26:27 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express'
 import dotenv from 'dotenv'
 import Routes from './routes/index'
-
+import cors from 'cors'
 dotenv.config()
 
 const app: Application = express()
@@ -11,5 +11,7 @@ const port = process.env.PORT || 3100
 app.use(express.json())
 
 app.use(Routes)
+
+app.use(cors({ origin: '*' }))
 
 app.listen(port, () => console.log('app listen port' + port))
