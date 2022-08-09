7c9774d2c0f58731d66a24acc2e359523efe44e4
haolegumi <hao.le@gumiviet.com>
2022-08-09 15:06:16 +0700

    fix api get user and add middleware morgan create file log in production

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,27 @@ import express, { Application } from 'express'
 import dotenv from 'dotenv'
 import Routes from './routes/index'
 import cors from 'cors'
+import helmet from 'helmet'
+import { createStream } from 'rotating-file-stream'
+import morgan from 'morgan'
 
 // custom components
 import CorsMiddleWare from './middlewares/cors.middleware'
 import { connectDb } from './configs/db.connect'
+
 dotenv.config()
 
 const app: Application = express()
 
 const port = process.env.PORT || 3100
+
+const accessLogStream = createStream('file.log', {
+  size: '10M', // rotate every 10 MegaBytes written
+  interval: '1d', // rotate daily
+  compress: 'gzip', // compress rotated files
+})
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 const main = () => {
   app.use(
@@ -22,6 +34,13 @@ const main = () => {
   app.use(CorsMiddleWare)
 
   app.use(express.json())
+
+  app.use(helmet())
+  app.use(
+    isProduction
+      ? morgan('combined', { stream: accessLogStream })
+      : morgan('dev')
+  )
 
   app.use(Routes)
 
