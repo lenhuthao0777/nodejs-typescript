bc70881fbf2a858a4f90cc953fa154d32c288b76
haolegumi <hao.le@gumiviet.com>
2022-08-11 17:33:32 +0700

    feature(#shop-shoes-03): add api create product 50%

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { ProductType } from 'src/@types/product.type'
 
 // import productModel from '../models/product.model'
 
@@ -7,8 +8,8 @@ dotenv.config()
 
 export const createProduct = async (req: Request, res: Response) => {
   try {
-    // const { title } = req.body
-    res.status(200).json({ message: 'abc' })
+    const body: ProductType = req.body
+    res.status(200).json({ message: 'abc', body: body.name })
   } catch (error) {
     res.status(500).json({ message: 'Create product failed!', code: error })
   }
